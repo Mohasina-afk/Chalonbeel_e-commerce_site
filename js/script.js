979253360f3ce905a6ef5a4fbf45c111b56328e0
Mohasina Afk <prantyafrin@gmail.com>
2020-11-16 18:54:52 +0600

    last commit

--- js/script.js
+++ js/script.js
@@ -1,136 +1,140 @@
-$(".navbar").hide();
-$(".bar").click(function(){
-    $(".navbar").fadeToggle();
-});
+$(document).ready(function () {
+    $(".navbar").hide();
+    $(".bar").click(function () {
+        $(".navbar").fadeToggle();
+    });
 
 
 
-$(".sidebar").hide();
-$(".udhaoIcon").click(function(){
-    $(".sidebar").fadeToggle();
-});
+    $(".udhaoIcon").click(function () {
+        $(".sidebar-active").toggle('.show-hide');
+    });
 
 
-//banner js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-$('#banner-part').slick({
-    dots: false,
-    infinite: true,
-    speed: 3000,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: true,
-    prevArrow: '<i class="fas fa-dove prev"></i>',
-    nextArrow: '<i class="fas fa-dove next"></i>',
-    responsive: [
-        {
-            breakpoint: 991,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
+
+
+    //banner js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    $('#banner-part').slick({
+        dots: false,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        prevArrow: '<i class="fas fa-dove prev"></i>',
+        nextArrow: '<i class="fas fa-dove next"></i>',
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-    },
-        {
-            breakpoint: 767,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+        ]
+    });
+
+
+
+
+    //banner-snd part js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    $('.slider').slick({
+        dots: false,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true,
+        prevArrow: '<i class="fas fa-arrow-left prev"></i>',
+        nextArrow: '<i class="fas fa-arrow-right next"></i>',
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: false
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-    },
-        {
-            breakpoint: 575,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
+        ]
+    });
+
+
+
+    //category part js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+    $('.cateslider_one').slick({
+        dots: false,
+        infinite: true,
+        speed: 3000,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: false,
+        responsive: [{
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    arrows: true,
+                    prevArrow: '<i class="fas fa-arrow-left prev"></i>',
+                    nextArrow: '<i class="fas fa-arrow-right next"></i>',
+                }
+            },
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    prevArrow: '<i class="fas fa-arrow-left prev"></i>',
+                    nextArrow: '<i class="fas fa-arrow-right next"></i>',
+                }
+            },
+            {
+                breakpoint: 575,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    arrows: true,
+                    prevArrow: '<i class="fas fa-arrow-left prev"></i>',
+                    nextArrow: '<i class="fas fa-arrow-right next"></i>',
+                }
             }
-    }
-  ]
-});
-
-
-
-
-
-
-
-
-//banner-snd part js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-$('.slider').slick({
-    dots: false,
-    infinite: true,
-    speed: 3000,
-    slidesToShow: 3,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: true,
-    prevArrow: '<i class="fas fa-arrow-left prev"></i>',
-    nextArrow: '<i class="fas fa-arrow-right next"></i>',
-    responsive: [
-        {
-            breakpoint: 991,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-            }
-    },
-        {
-            breakpoint: 767,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-    },
-        {
-            breakpoint: 575,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-    }
-  ]
-});
-
-
-
-//banner-snd part js @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
-$('.cateslider_one').slick({
-    dots: false,
-    infinite: true,
-    speed: 3000,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 3000,
-    arrows: false,
-    responsive: [
-        {
-            breakpoint: 991,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: false
-            }
-    },
-        {
-            breakpoint: 767,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-    },
-        {
-            breakpoint: 575,
-            settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-            }
-    }
-  ]
+        ]
+    });
 });
